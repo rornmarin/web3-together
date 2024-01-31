39d39070fd70a3b45772a489355e9bec4e71f68a
nakamurakei0978 <nakamurakei0978@gmail.com>
2024-01-31 16:32:24 +0700

    learnMoreButton

--- src/components/ButtonLearnMore.js
+++ src/components/ButtonLearnMore.js
@@ -4,10 +4,11 @@ import { IoIosArrowForward } from "react-icons/io";
 export const ButtonLearnMore = ({onClick, className}) => {
     return (
         <button onClick={onClick}
-            className={`${className} text-[20px] font-normal leading-[24.2px] rounded-[7px] py-[10.5px] px-[18.5px] ring-[2px] ring-white/50 flex items-center relative overflow-clip pr-[63px]`}
+            className={`${className} text-[20px] font-normal leading-[24.2px] rounded-[7px] py-[10.5px] px-[18.5px] ring-[2px] ring-white/50 flex items-center relative overflow-clip pr-[63px] hover:text-[#F2A93B] duration-100 hover:ring-[#F2A93B]`}
         >
             Learn More
             <IoIosArrowForward className='absolute bg-white/20 right-0 h-full w-[49px] flex justify-center items-center'/>
         </button>
     )
 }
+
