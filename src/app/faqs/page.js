775ad87aed37a9742b831006c8c09a503902ee1c
nakamurakei0978 <nakamurakei0978@gmail.com>
2024-02-02 11:56:59 +0700

    add page faqs with accordion

--- src/app/faqs/page.js
+++ src/app/faqs/page.js
@@ -1,8 +1,21 @@
+import { Accordion } from '@/components/faqs/Accordion'
 import React from 'react'
 
 const page = () => {
+  const faqsData = [{
+    question: 'How to mint 99x Club NFT Pass?',
+    answer: 'There will be 3333 99x Club NFT Passes available. Whitelist Mint Price will be 0.1 ETH per pass. Whitelist Mint Date : 20 Feb-23 13:30 UTC. Mint Link will be updated on httos://99x.club'
+  },{
+    question: 'How to mint 99x Club NFT Pass?',
+    answer: 'There will be 3333 99x Club NFT Passes available. Whitelist Mint Price will be 0.1 ETH per pass. Whitelist Mint Date : 20 Feb-23 13:30 UTC. Mint Link will be updated on httos://99x.club'
+  },{
+    question: 'How to mint 99x Club NFT Pass?',
+    answer: 'There will be 3333 99x Club NFT Passes available. Whitelist Mint Price will be 0.1 ETH per pass. Whitelist Mint Date : 20 Feb-23 13:30 UTC. Mint Link will be updated on httos://99x.club'
+  }]
   return (
-    <div>FAQs</div>
+    <div className='mt-10 grid gap-5'>
+      <Accordion data={faqsData} />
+    </div>
   )
 }
 
