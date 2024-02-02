edc5baa2082bec991aaff14e6200e25f4d76c1d5
Rorn Marin <rornmarin9@gmail.com>
2024-02-02 10:06:13 +0700

    add community page and make responsive

--- src/app/community/joins/page.js
+++ src/app/community/joins/page.js
@@ -1,29 +1,39 @@
+"use client";
 import React from "react";
 import { SmallEvenCard } from "@/components/SmallEvenCard";
+import { usePathname } from "next/navigation";
 
-export const JoinPage = () => {
+const JoinEvent = () => {
+  const pathname = usePathname();
+
+  const handleClickBack = () => {
+    window.location.href = "/community";
+  };
   return (
-    <div className="w-full h-[1306px] flex justify-center">
-      <div className="">
+    <div className="w-full flex justify-center">
+      <div className="w-full ">
         <div className="flex flex-col items-center justify-center md:flex-row mb-5 md:mb-20">
-          <h1 className="md:w-full w-[590px] mx-auto text-center font-inter font-bold text-2xl md:text-3xl lg:text-4xl  tracking-widest">
+          <h1 className="mx-auto text-center font-inter font-bold text-base md:text-base lg:text-base tracking-widest">
             If you hold 1 Web3 Together Membership <br></br>Pass = 1 Free NFT in
             Each NFT Project
           </h1>
-          <button className="bg-[#FFFFFF] bg-opacity-20 w-[127px] h-[56px] mr-10 md:mr-4 rounded-lg text-[#F2A93B] font-inter font-bold text-base md:text-xl leading-7 tracking-wider self-end mt-4 md:mt-0">
+          <button
+            onClick={handleClickBack}
+            className="bg-[#FFFFFF] bg-opacity-20 w-32 h-14 mr-10 md:mr-4 rounded-lg text-[#F2A93B] font-inter font-bold text-base md:text-xl leading-7 tracking-wider self-end mt-4 md:mt-0"
+          >
             Back
           </button>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2">
-          <div className="relative mb-8 items-center justify-center md:items-center md:justify-center ">
+          <div className="relative mb-8 items-center justify-center md:items-center md:justify-self-end ">
             <img
-              className="h-[407px] md:w-[610px] rounded-lg mb-4 md:mb-0 px-10 md:px-0"
+              className="h-3/6  rounded-lg mb-4 mx-auto pl-72 md:pl-0"
               src="https://s3-alpha-sig.figma.com/img/1bc5/cc4c/c0847ed3e7424e2c02351b7eef36211d?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=e4PdcvReLaP5i0QLiSb8s8Dztngsvqi~6yCgs2s5wWaEiCsm-wTT36PjtrPN8RQTZkXt~09OLH0gRRDakM3a9CRWs8ZL~OP3Yf17pI3F0oWltqLDzB~EBkzbkZIt6l4rwuvgP5wiCt983aSGerPPDDasBSG0yIXTRMvpdskubEQGwaRQ-wt3e58X~n-gqJUCIf65pBDBMDG3daY7KtydnMSF19D3zetWQjlJFs3d9yvwgC1pmpIe~ZaTkcROrGd6R7C5l6yoYZOvarE1y3PSLgHjIOGE1CpjXgFXA1Rz~F46kNzEfuIbO3pCN8YgU~laTUSP3dXG1JM4u1xuA0hFYQ__"
               alt="Event Image"
             />
-            <div className="flex flex-col md:flex-row items-center w-[610px] justify-between md:justify-between">
-              <div className="relative top-[140px] md:top-[170px]">
+            <div className="mx-auto flex flex-col md:flex-row items-center justify-between md:justify-between">
+              <div className="relative top-32 md:top-36 mr-0 md:mr-2">
                 <SmallEvenCard
                   img="https://s3-alpha-sig.figma.com/img/1bc5/cc4c/c0847ed3e7424e2c02351b7eef36211d?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=e4PdcvReLaP5i0QLiSb8s8Dztngsvqi~6yCgs2s5wWaEiCsm-wTT36PjtrPN8RQTZkXt~09OLH0gRRDakM3a9CRWs8ZL~OP3Yf17pI3F0oWltqLDzB~EBkzbkZIt6l4rwuvgP5wiCt983aSGerPPDDasBSG0yIXTRMvpdskubEQGwaRQ-wt3e58X~n-gqJUCIf65pBDBMDG3daY7KtydnMSF19D3zetWQjlJFs3d9yvwgC1pmpIe~ZaTkcROrGd6R7C5l6yoYZOvarE1y3PSLgHjIOGE1CpjXgFXA1Rz~F46kNzEfuIbO3pCN8YgU~laTUSP3dXG1JM4u1xuA0hFYQ__"
                   title="TTN Event"
@@ -32,7 +42,7 @@ export const JoinPage = () => {
                   joinLink="/community/join"
                 />
               </div>
-              <div className="relative top-[170px]">
+              <div className="relative top-36 md:top-36 ">
                 <SmallEvenCard
                   img="https://s3-alpha-sig.figma.com/img/1bc5/cc4c/c0847ed3e7424e2c02351b7eef36211d?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=e4PdcvReLaP5i0QLiSb8s8Dztngsvqi~6yCgs2s5wWaEiCsm-wTT36PjtrPN8RQTZkXt~09OLH0gRRDakM3a9CRWs8ZL~OP3Yf17pI3F0oWltqLDzB~EBkzbkZIt6l4rwuvgP5wiCt983aSGerPPDDasBSG0yIXTRMvpdskubEQGwaRQ-wt3e58X~n-gqJUCIf65pBDBMDG3daY7KtydnMSF19D3zetWQjlJFs3d9yvwgC1pmpIe~ZaTkcROrGd6R7C5l6yoYZOvarE1y3PSLgHjIOGE1CpjXgFXA1Rz~F46kNzEfuIbO3pCN8YgU~laTUSP3dXG1JM4u1xuA0hFYQ__"
                   title="TTN Event"
@@ -43,14 +53,14 @@ export const JoinPage = () => {
               </div>
             </div>
           </div>
-          <div className="w-full md:w-[587px] pl-3 md:pl-10 mt-10 mb-10 md:mt-0">
-            <h1 className="font-sans text-base text-white tracking-wider leading-loose">
+          <div className="md:w-4/6 w-5/6 pl-3 md:pl-10 mt-72 mb-10 md:mt-0">
+            <h1 className="font-sans text-base text-white tracking-wider leading-8">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
             </h1>
-            <h1 className="mt-5 font-inter font-bold text-xl md:text-2xl leading-[40px] tracking-widest">
+            <h1 className="mt-5 w-5/6 font-inter font-bold text-xl md:text-2xl leading-snug tracking-widest">
               How can I join?
             </h1>
             <h1 className="font-sans text-base leading-loose text-white tracking-wider">
@@ -58,10 +68,10 @@ export const JoinPage = () => {
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s,
             </h1>
-            <h1 className="mt-5 font-inter font-bold text-xl md:text-2xl leading-[40px] tracking-widest">
+            <h1 className="mt-5 font-inter font-bold text-xl md:text-2xl leading-snug tracking-widest">
               Timeline
             </h1>
-            <h1 className="w-full md:w-[473px] font-sans text-base text-white tracking-wider leading-loose">
+            <h1 className="w-full md:w-4/5 font-sans text-base text-white tracking-wider leading-loose">
               <ul className="list-disc pl-4">
                 <li>27 March 2023: Open application</li>
                 <li>
@@ -78,7 +88,7 @@ export const JoinPage = () => {
               </span>
               https//:www.event.com
             </h1>
-            <h1 className="w-full md:w-[427px] font-sans text-base text-white tracking-wider leading-loose">
+            <h1 className="w-full md:w-4/5 font-sans text-base text-white tracking-wider leading-loose">
               <span className="font-inter font-bold text-xl md:text-2xl leading-[40px] tracking-widest">
                 Noted:
               </span>
@@ -93,4 +103,4 @@ export const JoinPage = () => {
   );
 };
 
-
+export default JoinEvent;
--- src/components/SmallEvenCard.js
+++ src/components/SmallEvenCard.js
@@ -3,36 +3,38 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-export const SmallEvenCard = ({ img, title, description, date, joinLink }) => {
+export const SmallEvenCard = ({
+  img,
+  title,
+  description,
+  date,
+  joinLink,
+  className,
+}) => {
   const pathname = usePathname();
   return (
     <div>
-      <div className="relative h-[376px]">
+      <div className={`${className ? className : "relative h-96 w-96"} `}>
         <img
-          className="flex bottom-[320px] z-30 w-[295px] h-[172px] rounded-lg object-cover absolute"
+          className="flex bottom-80 z-30 h-44 w-96 rounded-lg object-cover absolute"
           src={img}
           alt="Event Image"
         />
-        <div className="h-[253px] w-[295px] bg-white bg-opacity-10 rounded-lg relative">
-          <div className="absolute top-[70px] mx-[13px]">
+        <div className="h-64 w-96 bg-white bg-opacity-10 rounded-lg relative">
+          <div className="absolute w-96 top-20 mx-5">
             <h1 className="font-inter font-bold text-xl mb-2 tracking-widest">
               {title}
             </h1>
-            <p className="font-inter font-normal text-base leading-7 tracking-wider">
+            <p className="w-72 font-inter font-normal text-base leading-7 tracking-wider">
               {description}
             </p>
-            <div className="flex flex-row justify-between pt-[25px]">
+            <div className="flex flex-row justify-between pt-5">
               <h1 className="font-inter font-normal text-base leading-7 text-white text-opacity-40 tracking-wider">
                 {date}
               </h1>
               <Link
                 href={joinLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`${
-                  pathname === "/community/join" &&
-                  "text-[#F2A93B] font-inter font-bold text-base leading-7 tracking-wider"
-                }`}
+                className="text-[#F2A93B] mr-10 font-inter font-bold text-base leading-7 tracking-wider"
               >
                 Join Now
               </Link>
